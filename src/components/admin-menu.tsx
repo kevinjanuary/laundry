abadb60809123e4200805cc806f00b707ade4ace
Kevin Januari <128971428+kevinjanuary@users.noreply.github.com>
2024-01-25 23:53:44 +0700

    styling and refactor

--- src/components/admin-menu.tsx
+++ src/components/admin-menu.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { cn } from "@/lib/utils"
-import { Banknote, Box } from "lucide-react"
+import { Banknote, Box, Home } from "lucide-react"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 
@@ -11,11 +11,20 @@ const AdminMenu = () => {
   const isActive = (href: string) => pathname === href
 
   return (
-    <div className="flex mt-auto border-t-2">
+    <div className="flex mt-auto border-t-2 bg-white">
+      <Link
+        href="/admin/dashboard"
+        className={cn(
+          "w-1/3 flex items-center justify-center p-4",
+          !isActive("/admin/dashboard") && "text-muted-foreground"
+        )}
+      >
+        <Home size={24} />
+      </Link>
       <Link
         href="/admin/dashboard/products"
         className={cn(
-          "w-1/2 flex items-center justify-center p-4",
+          "w-1/3 flex items-center justify-center p-4",
           !isActive("/admin/dashboard/products") && "text-muted-foreground"
         )}
       >
@@ -24,7 +33,7 @@ const AdminMenu = () => {
       <Link
         href="/admin/dashboard/orders"
         className={cn(
-          "w-1/2 flex items-center justify-center p-4",
+          "w-1/3 flex items-center justify-center p-4",
           !isActive("/admin/dashboard/orders") && "text-muted-foreground"
         )}
       >
--- src/components/logout-button.tsx
+++ src/components/logout-button.tsx
@@ -3,7 +3,11 @@
 import { ChevronRight, LogOut } from "lucide-react"
 import { signOut } from "next-auth/react"
 
-export const LogoutButton = () => {
+export const LogoutButton = ({
+  hideRightIcon = false,
+}: {
+  hideRightIcon?: boolean
+}) => {
   return (
     <div
       className="flex gap-2 items-center text-destructive cursor-pointer"
@@ -13,7 +17,9 @@ export const LogoutButton = () => {
     >
       <LogOut width={24} height={24} />
       <span>Logout</span>
-      <ChevronRight width={20} height={20} className="ml-auto" />
+      {!hideRightIcon && (
+        <ChevronRight width={20} height={20} className="ml-auto" />
+      )}
     </div>
   )
 }
