a67cdd1f3e606bbb9d60d8f82b6232a71875a94c
Kevin Januari <128971428+kevinjanuary@users.noreply.github.com>
2024-01-26 13:36:37 +0700

    styling, logout button, dashboard information

--- src/app/(admin)/admin/dashboard/layout.tsx
+++ src/app/(admin)/admin/dashboard/layout.tsx
@@ -1,9 +1,14 @@
 import AdminMenu from "@/components/admin-menu"
+import { LogoutButton } from "@/components/logout-button"
 
 const AdminDashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full flex flex-col">
-      {children}
+      <div className="flex justify-between gap-2 h-10 items-center px-6 py-8 border-b">
+        <h1>Admin Dashboard</h1>
+        <LogoutButton hideRightIcon />
+      </div>
+      <div className="p-6 bg-white overflow-scroll">{children}</div>
       <AdminMenu />
     </div>
   )
